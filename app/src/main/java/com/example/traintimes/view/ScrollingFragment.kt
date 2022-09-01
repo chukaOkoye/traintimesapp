916a3aceb49234a80856378e9dc0caa754fe17ee
Chuka Okoye-Ahaneku <chuka.okoye-ahaneku@clearscore.com>
2022-09-01 17:00:48 +0100

    feat: add gradle settings

--- app/src/main/java/com/example/traintimes/view/ScrollingFragment.kt
+++ app/src/main/java/com/example/traintimes/view/ScrollingFragment.kt
@@ -5,14 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.traintimes.R
 
-class ScrollingFragment : Fragment() {
+class ScrollingFragment : Fragment(R.layout.fragment_scrolling) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_scrolling, container, false)
-    }
 }
